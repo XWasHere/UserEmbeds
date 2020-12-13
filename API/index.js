4a69a23308234cdeb4bbccbdfddf931f7c237119
X-San <70487315+XWasHere@users.noreply.github.com>
2020-12-13 15:16:38 -0500

    bye

--- API/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-module.exports = {
-    'util': require('./util'),
-    'embed': require('./embed')
-};
--- API/util/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const out = require('./out');
-
-module.exports = {
-    'print': out.print,
-    'printIf':out.printIf,
-    'printD':out.debugPrint
-};
